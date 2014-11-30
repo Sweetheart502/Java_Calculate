33d96ff61d2ff86e5507720e2b91d643a379390b
gaopu <936302529@qq.com>
2014-11-30 21:55:45 +0800

    每个人的方法单独到一个文件中,最后再合并运行

---
+++ com/xiyoulinux/calculate/split/gaopu/gaopu.java
@@ -0,0 +1,5 @@
+//	生成新的一个List返回逆波兰表达式
+	public List<String> toSuffixExpression(List<String> normalExpression) {
+		getPriority("D:/priority.txt");
+		return null;
+	}
---
+++ com/xiyoulinux/calculate/split/liyingxiao/liyingxiao.java
@@ -0,0 +1,4 @@
+//	检查输入的数学表达式是否有错
+	public boolean checkError() {
+		return false;
+	}
