84d9442d74804f4a5d12be5c9d152706dbb5abc6
gaopu <936302529@qq.com>
2014-12-03 19:47:52 +0800

    修改toNormalExpression接口参数&高朴上传更新

--- com/xiyoulinux/calculate/Calculate.java
+++ com/xiyoulinux/calculate/Calculate.java
@@ -50,7 +50,7 @@ public class Calculate {
     }
 
     //	把原始数据中的求和，平均数等处理成数学式子。并且输入不规范的改成规范的（例如“.7”改为“0.7”）
-    public List<String> toNormalExpression() {
+    public List<String> toNormalExpression(String expression) {
         return null;
     }
 
--- com/xiyoulinux/calculate/split/gaopu/gaopu.java
+++ com/xiyoulinux/calculate/split/gaopu/gaopu.java
@@ -1,8 +1,7 @@
 //	生成新的一个List返回逆波兰表达式
-	public List<String> toSuffixExpression(List<String> normalExpression) {
-        //首先是处理数学函数内部的东西,例如log(2,long(2,32))---->log(2,5)
-
-
+    public List<String> toSuffixExpression(List<String> normalExpression) {
+        //首先是化简函数
+        simpleFunction(normalExpression);
         //========================================================================================================
         //下面是转后缀表达式
         getPriority("/home/geekgao/priority");//获取运算符优先级
@@ -42,4 +41,55 @@
         }
 
         return suffixExpression;
+    }
+    
+    //处理数学函数内部的东西,例如log(2,log(2,30 + 2))---->log(2,5)
+    private void simpleFunction(List<String> normalExpression) {
+        Set<String> operator = operatorPriority.keySet();//得到运算符集合
+        for (int i = 0;i < normalExpression.size();i++) {
+            String currentStr = normalExpression.get(i);
+            //=============================================================================
+            //下面排除不用处理这个节点的情况
+
+            //(1)这个节点是运算符那么就不用处理
+            if (operator.contains(currentStr) || currentStr.equals("(") || currentStr.equals(")")) {
+                continue;
+            }
+
+            //(2)这个节点没有函数不用处理
+            String[] mathFunctionArray = (String[]) mathFunction.toArray();//获得数学函数数组,比较当前节点是否含有数学函数
+            int j;
+            //判断这个节点是否含有函数
+            for (j = 0; j < mathFunctionArray.length && !currentStr.contains(mathFunctionArray[j]); j++)
+                ;
+            if (j == mathFunctionArray.length) {
+                continue;
+            }
+
+            //(3)到这了说明含有函数,函数内部没有函数并且没有运算符也不用计算
+            String functionInnerStr = currentStr.substring(currentStr.indexOf('('), currentStr.lastIndexOf(')'));
+            String [] operatorArray = (String[]) operator.toArray();//获得运算符数组,比较函数内部是否含有运算符
+
+            //判断函数内部是否含有函数
+            for (j = 0; j < mathFunctionArray.length && !functionInnerStr.contains(mathFunctionArray[j]); j++)
+                ;
+            int k;
+            //判断函数内部是否含有运算符
+            for (k = 0; k < operatorArray.length && !functionInnerStr.contains(operatorArray[k]); k++)
+                ;
+
+            if (j == mathFunctionArray.length && k == operatorArray.length) {
+                continue;
+            }
+            //=============================================================================
+            //函数内部有需要计算的东西,需要把里面的计算出来替换函数内部的东西
+            String [] functionParam = functionInnerStr.split(",");//有的函数有两个参数,按照逗号分开的,每一部分都处理
+            //处理办法是给函数的参数后面加一个"+0",然后进行同样的流程获得最后的结果放进函数参数的相应位置
+            for (int m = 0;m < functionParam.length;m++) {
+                functionParam[m] = functionParam[m].concat("+0");
+                String resultStr = calculate(toSuffixExpression(toNormalExpression(functionParam[m])));
+                currentStr = currentStr.replace(functionParam[m],resultStr);
+            }
+            normalExpression.set(i,currentStr);
+        }
     }
